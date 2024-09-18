b3ce0fd9090cec0aa2b54006bf822346beffdb93
Kruglov Leonid <lenya@leshak.ru>
2024-09-19 00:08:12 +0300

    update

--- projects/triangle/main.go
+++ projects/triangle/main.go
@@ -1,7 +1,9 @@
 package main
-
 import "fmt"
+import "math"
 
 func main() {
-	fmt.Println("Hello, world!!!")
+    var a, b int
+    fmt.Scan(&a, &b)
+    fmt.Print(math.Sqrt(float64(a * a + b * b)))
 }
